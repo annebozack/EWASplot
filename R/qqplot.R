ff2b0ed66720e69ead24bbf157c9be0c6e916105
annebozack <anne.bozack@gmail.com>
2021-07-22 11:52:51 -0400

    First commit

---
+++ R/qqplot.R
@@ -0,0 +1,25 @@
+#' Compute genomic inflation factor lambda
+#'
+#' This function computes the genomic inflation factor (λ) using the median approach.
+#'
+#' @param pvector Vector of p-values
+#' @return Value of λ
+#' @export
+lambda = function(p) {
+	return(median(qchisq(p, df=1, lower.tail=FALSE), na.rm=TRUE) / qchisq(0.5, df=1))
+	}
+
+#' Plot Q-Q plot
+#'
+#' This function creates a quantile-quantile (Q-Q) plot of p-values using ggplot2.
+#'
+#' @param pvector Vector of p-values
+#' @return Q-Q plot
+#' @export
+qqplot = function(pvector){
+	l = round(lambda(pvector), 3)
+	o = -log10(sort(pvector, decreasing = FALSE))
+	e = -log10(ppoints(length(pvector)))
+	df = data.frame(o = o, e = e)
+	ggplot2::ggplot(df, aes(e, o)) + geom_point(alpha = 0.5, size = 1) + geom_abline(intercept = 0, slope = 1, color = '#AB3428') + labs(y = expression(Observed ~ ~-log[10](italic(p))), x = expression(Expected ~ ~-log[10](italic(p)))) + theme_classic() + annotate("text", x = 1, y = 5, label = paste0('lambda = ', l))
+}
